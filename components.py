Fixed rank calculation Added sending/receiving history at nodes Broadcast delivers now the same packet to every node

components.py
@@ -5,6 +5,51 @@ import numpy as np
 import logging
 
 
+def determinant(matrix, size, fieldsize):
+    """Solve determinant with laplace."""
+    c = []
+    d = 0
+    if size == 0:
+        for i in range(len(matrix)):
+            d = (d + matrix[i]) % fieldsize
+        return d
+    elif len(matrix) == 1:
+        return matrix[0]
+    elif len(matrix) == 4:
+        c = ((matrix[0] * matrix[3]) - (matrix[1] * matrix[2])) % fieldsize
+        return c
+    else:
+        for j in range(size):
+            new_matrix = []
+            for i in range(size*size):
+                if i % size != j and i >= size:
+                    new_matrix.append(matrix[i])
+
+            c.append((determinant(new_matrix, size - 1, fieldsize) * matrix[j] * ((-1) ** (j + 2))) % fieldsize)
+        d = determinant(c, 0, fieldsize)
+        return d
+
+
+def calcrank(matrix, fieldsize):
+    """Another try."""
+    if not len(matrix):
+        return 0
+    elif len(matrix) == 1:
+        return 1
+    for i in range(len(matrix[:, 0]), 0, -1):
+        for j in range(2 ** len(matrix[0])):
+            binj = [int(x) for x in bin(j)[2:]]
+            if sum(binj) == i:
+                linematrix = []
+                binj = [0] * (len(matrix[0]) - len(binj)) + binj
+                for linenumber in range(i):
+                    for idx, binv in enumerate(binj):
+                        if binv:
+                            linematrix.append(matrix[linenumber][idx])
+                if determinant(linematrix, i, fieldsize):
+                    return i
+
+
 class Node:
     """Representation of a node on the network."""
 
@@ -21,13 +66,16 @@ class Node:
         self.complete = (name == 'S')
         self.trash = []
         self.quiet = False
+        self.history = []
+        self.sendhistory = []
 
     def __str__(self):
         return str(self.name)
 
-    def buffpacket(self, batch=0, coding=None, preveotx=0, special=False):
+    def buffpacket(self, batch=0, coding=None, preveotx=0, special=False, ts=0):
         """Buffer incoming packets so they will be received at end of time slot."""
         self.incbuffer.append((batch, coding, preveotx, special))
+        self.history.append((batch, coding, preveotx, special, ts))
 
     def becomesource(self):
         """Act like source. Will be triggered if all neighbors are complete."""
@@ -37,7 +85,7 @@ class Node:
 
     def isdone(self):
         """Return True if able to decode."""
-        return self.complete
+        return (self.coding == calcrank(self.buffer, self.fieldsize)) or self.name == 'S'
 
     def getbatch(self):
         """Return current batch."""
@@ -53,25 +101,25 @@ class Node:
                 coding = np.random.randint(self.fieldsize, size=self.coding)
             return coding
         elif self.creditcounter > 0:  # Check tx credit
-            if self.complete or self.isdone():
+            if self.isdone():
                 coding = [0]
                 while sum(coding) == 0:  # Use random coding instead of recoding if rank is full
                     coding = np.random.randint(self.fieldsize, size=self.coding)
                 return coding
-            recoded = []
-            modbuffer = np.array([], dtype=int)
-            for line in self.buffer:
-                newline = [0]
-                if len(modbuffer):
-                    while sum(newline) == 0:
+            recoded = [0]
+            while sum(recoded) == 0:
+                modbuffer = np.array([], dtype=int)
+                for line in self.buffer:
+                    if len(modbuffer):
                         newline = (np.random.randint(self.fieldsize) * line) % self.fieldsize
-                    modbuffer = np.vstack([modbuffer, newline])
-                else:
-                    while sum(newline) == 0:
+                        modbuffer = np.vstack([modbuffer, newline])
+                    else:
                         newline = (np.random.randint(self.fieldsize) * line) % self.fieldsize
-                    modbuffer = np.array([newline])
-            for i in range(len(self.buffer[0])):
-                recoded.append(sum(modbuffer[:, i]) % self.fieldsize)  # Recode to get new packet
+                        modbuffer = np.array([newline])
+                recoded = []
+                for i in range(len(self.buffer[0])):
+                    recoded.append(sum(modbuffer[:, i]) % self.fieldsize)  # Recode to get new packet
+            self.sendhistory.append(recoded)
             return np.array(recoded)
         else:
             return None
@@ -92,6 +140,13 @@ class Node:
         """Return true if quiet."""
         return self.quiet
 
+    def getrank(self):
+        """Return current rank."""
+        if self.name == 'S':
+            return self.coding
+        else:
+            return calcrank(self.buffer, self.fieldsize)
+
     def gettrash(self, maxts):
         """Return trash."""
         x, y = np.unique(self.trash, return_counts=True)
@@ -110,6 +165,7 @@ class Node:
         """Make destination awaiting new batch."""
         self.batch += 1
         self.buffer = np.array([], dtype=int)
+        # self.complete = False
         self.quiet = False
 
     def rcvpacket(self, timestamp):
@@ -117,17 +173,18 @@ class Node:
         for batch, coding, preveotx, special in self.incbuffer:
             if self.name == 'S':  # Cant get new information if you're source
                 return
-            elif batch != self.batch or not len(self.buffer):  # Delete it if you're working on deprecated batch
+            elif batch > self.batch or not len(self.buffer):  # Delete it if you're working on deprecated batch
                 self.buffer = np.array([coding], dtype=int)
-                if self.creditcounter == float('inf'):      # Return to normal if new batch arrives
+                self.batch = batch
+                if self.creditcounter == float('inf'):  # Return to normal if new batch arrives
                     self.creditcounter = 0.
                 if self.quiet:
                     self.quiet = False
             else:  # Just add new information if its new
-                if np.linalg.matrix_rank(self.buffer) < np.linalg.matrix_rank(np.vstack([self.buffer, coding])):
+                if calcrank(self.buffer, self.fieldsize) < calcrank(np.vstack([self.buffer, coding]), self.fieldsize):
                     self.buffer = np.vstack([self.buffer, coding])
                 else:
-                    if self.complete or self.isdone():
+                    if self.isdone():
                         logging.info('Got linear dependent packet at {}, decoder full time = {}'.format(self.name,
                                                                                                         timestamp))
                         self.trash.append(timestamp)
@@ -141,8 +198,8 @@ class Node:
             if preveotx > self.eotx:
                 self.creditcounter += self.credit
         self.incbuffer = []
-        if self.name != 'S' and not self.complete:
-            self.complete = self.coding == np.linalg.matrix_rank(self.buffer)
+        if self.name != 'S':
+            self.complete = self.coding == calcrank(self.buffer, self.fieldsize)
 
     def reducecredit(self):
         """Reduce tx credit."""